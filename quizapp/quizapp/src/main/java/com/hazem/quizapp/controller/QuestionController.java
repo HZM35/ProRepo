a133b96ad99b9a2fa21feff3d56d95b595c7217a
Hazem Hussin <102246134+HZM35@users.noreply.github.com>
2023-09-19 12:32:36 +0200

    Quiz App upload

---
+++ quizapp/quizapp/src/main/java/com/hazem/quizapp/controller/QuestionController.java
@@ -0,0 +1,38 @@
+package com.hazem.quizapp.controller;
+
+import com.hazem.quizapp.model.Question;
+import com.hazem.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+    @RestController
+    @RequestMapping("question")
+    public class QuestionController {
+        @Autowired
+        QuestionService questionService;
+
+            @GetMapping("allQuestions")
+            public ResponseEntity <List <Question>> getAllQuestions(){
+                return questionService.getAllQuestions();
+            }
+            @GetMapping("category/{category}")
+            public ResponseEntity< List <Question>> getQuestionsByCategory( @PathVariable String category){
+                return questionService.getQuestionsByCategory(category);
+
+            }
+            @PostMapping("add")
+            public ResponseEntity <String> addQuestion(@RequestBody Question question){
+            return  questionService.addQuestion(question);
+            }
+
+
+            @DeleteMapping("delete")
+            public String deleteQuestion (@RequestBody Question question){
+                    return  questionService.deleteQuestion(question);
+            }
+
+
+}
