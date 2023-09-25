49ab4efb37784ed54449346c106a8faf83d3090f
HZM35 <hazemhs35@gmail.com>
2023-09-25 10:46:45 +0200

    commit these changes

--- My personal page/mailSender.js
+++ My personal page/mailSender.js
@@ -1,24 +1,35 @@
- function sendMail(){
+const contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const sendMail = async () => {
     var params = {
-        name:document.getElementById("name").value ,
-        subject:document.getElementById("subject").value ,
-        email:document.getElementById("email").value ,
-        message:document.getElementById("message").value 
+      name: document.getElementById("name").value,
+      subject: document.getElementById("subject").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
     };
-    
+
     const serviceID = "service_bp6u6tc";
     const templateID = "template_44z4jkg";
-    
-    emailjs
-    .send(serviceID,templateID, params)
-    .then(
-            (res) => {
-            document.getElementById("name").value= "";
-            document.getElementById("subject").value= "";
-            document.getElementById("email").value= "";
-            document.getElementById("message").value= "";
-            console.log(res);
-            alert("Your message has been sent successfully");
-        })
-    .catch((err)=> console.log(err));
-}
+
+    // Prüfen, ob alle Formularfelder ausgefüllt sind.
+    if (params.name === "" || params.subject === "" || params.email === "" || params.message === "") {
+      alert("Bitte füllen Sie alle Felder aus.");
+      return;
+    }
+        if (!confirm("Are you sure you want to send this message?")) {
+      return;
+    }
+    await emailjs.send(serviceID, templateID, params);
+
+    document.getElementById("name").value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    console.log("Your message has been sent successfully");
+  };
+
+  sendMail();
+});
